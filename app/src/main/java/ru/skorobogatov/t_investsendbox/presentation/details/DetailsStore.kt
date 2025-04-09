f1ecabe459c221c02088ab1dfe7e918dd6e678b6
Abbat888 <zmeuka96@gmail.com>
2025-04-09 09:36:18 +0400

    edited FavouriteStore.kt and SearchStore.kt

--- app/src/main/java/ru/skorobogatov/t_investsendbox/presentation/details/DetailsStore.kt
+++ app/src/main/java/ru/skorobogatov/t_investsendbox/presentation/details/DetailsStore.kt
@@ -1,58 +1,282 @@
 package ru.skorobogatov.t_investsendbox.presentation.details
 
+import android.icu.util.Calendar
 import com.arkivanov.mvikotlin.core.store.Reducer
 import com.arkivanov.mvikotlin.core.store.Store
 import com.arkivanov.mvikotlin.core.store.StoreFactory
 import com.arkivanov.mvikotlin.extensions.coroutines.CoroutineBootstrapper
 import com.arkivanov.mvikotlin.extensions.coroutines.CoroutineExecutor
+import kotlinx.coroutines.launch
+import ru.skorobogatov.t_investsendbox.domain.entity.Candle
+import ru.skorobogatov.t_investsendbox.domain.entity.Instrument
+import ru.skorobogatov.t_investsendbox.domain.entity.Timeframe
+import ru.skorobogatov.t_investsendbox.domain.usecase.ChangeFavouriteStateUseCase
+import ru.skorobogatov.t_investsendbox.domain.usecase.GetCandlesUseCase
+import ru.skorobogatov.t_investsendbox.domain.usecase.ObserveFavouriteStateUseCase
 import ru.skorobogatov.t_investsendbox.presentation.details.DetailsStore.Intent
 import ru.skorobogatov.t_investsendbox.presentation.details.DetailsStore.Label
 import ru.skorobogatov.t_investsendbox.presentation.details.DetailsStore.State
+import javax.inject.Inject
 
-internal interface DetailsStore : Store<Intent, State, Label> {
+interface DetailsStore : Store<Intent, State, Label> {
 
     sealed interface Intent {
+
+        data object ClickBack : Intent
+
+        data object ClickChangeFavouriteStatus : Intent
+
+        data object ClickChangeTimeframe : Intent
+
+        data object ClickChangePeriod : Intent
+
+        data class TimeframeChanged(
+            val timeframe: Timeframe
+        ) : Intent
+
+        data class PeriodChanged(
+            val from: Long,
+            val to: Long
+        ) : Intent
     }
 
-    data class State(val todo: Unit)
+    data class State(
+        val instrument: Instrument,
+        val isFavourite: Boolean,
+        val candlesState: CandlesState,
+        val timeframeState: TimeframeState,
+        val periodState: PeriodState
+    ) {
+
+        sealed interface CandlesState {
+
+            data object Initial : CandlesState
+
+            data object Loading : CandlesState
+
+            data object Error : CandlesState
+
+            data class Loaded(
+                val candlesList: List<Candle>
+            ) : CandlesState
+        }
+
+        sealed interface TimeframeState {
+
+            data object OnChange : TimeframeState
+
+            data class SelectedTimeframe(
+                val timeframe: Timeframe
+            ) : TimeframeState
+        }
+
+        sealed interface PeriodState {
+
+            data object OnChange : PeriodState
+
+            data class SelectedPeriod(
+                val from: Long,
+                val to: Long
+            ) : PeriodState
+        }
+    }
 
     sealed interface Label {
+
+        data object ClickBack : Label
     }
 }
 
-internal class DetailsStoreFactory(
-    private val storeFactory: StoreFactory
+class DetailsStoreFactory @Inject constructor(
+    private val storeFactory: StoreFactory,
+    private val getCandlesUseCase: GetCandlesUseCase,
+    private val changeFavouriteStateUseCase: ChangeFavouriteStateUseCase,
+    private val observeFavouriteStateUseCase: ObserveFavouriteStateUseCase
 ) {
 
-    fun create(): DetailsStore =
+    fun create(instrument: Instrument): DetailsStore =
         object : DetailsStore, Store<Intent, State, Label> by storeFactory.create(
             name = "DetailsStore",
-            initialState = State(Unit),
-            bootstrapper = BootstrapperImpl(),
+            initialState = State(
+                instrument = instrument,
+                isFavourite = false,
+                candlesState = State.CandlesState.Initial,
+                timeframeState = State.TimeframeState.SelectedTimeframe(Timeframe.CANDLE_INTERVAL_30_MIN),
+                periodState = State.PeriodState.SelectedPeriod(
+                    from = Calendar.getInstance().timeInMillis - 86400000,
+                    to = Calendar.getInstance().timeInMillis - (86400000 * 3)
+                )
+            ),
+            bootstrapper = BootstrapperImpl(
+                instrument = instrument,
+                from = Calendar.getInstance().timeInMillis - 86400000,
+                to = Calendar.getInstance().timeInMillis - (86400000 * 3),
+                timeframe = Timeframe.CANDLE_INTERVAL_30_MIN
+            ),
             executorFactory = ::ExecutorImpl,
             reducer = ReducerImpl
         ) {}
 
     private sealed interface Action {
+
+        data class FavouriteStatusChanged(val isFavourite: Boolean) : Action
+
+        data class CandlesLoaded(val candlesList: List<Candle>) : Action
+
+        data object CandlesStartLoading : Action
+
+        data object CandlesLoadingError : Action
     }
 
     private sealed interface Msg {
+
+        data class FavouriteStatusChanged(val isFavourite: Boolean) : Msg
+
+        data class CandlesLoaded(val candlesList: List<Candle>) : Msg
+
+        data object CandlesStartLoading : Msg
+
+        data object CandlesLoadingError : Msg
+
+        data object TimeframeOnChange : Msg
+
+        data object PeriodOnChange : Msg
+
+        data class TimeframeChanged(
+            val timeframe: Timeframe
+        ) : Msg
+
+        data class PeriodChanged(
+            val from: Long,
+            val to: Long
+        ) : Msg
     }
 
-    private class BootstrapperImpl : CoroutineBootstrapper<Action>() {
+    private inner class BootstrapperImpl(
+        private val instrument: Instrument,
+        val from: Long,
+        val to: Long,
+        val timeframe: Timeframe
+    ) : CoroutineBootstrapper<Action>() {
+
         override fun invoke() {
+            scope.launch {
+                observeFavouriteStateUseCase(instrument.figi).collect {
+                    dispatch(Action.FavouriteStatusChanged(it))
+                }
+            }
+            scope.launch {
+                dispatch(Action.CandlesStartLoading)
+                val fromCalendar = Calendar.getInstance().also { it.timeInMillis = from }
+                val toCalendar = Calendar.getInstance().also { it.timeInMillis = to }
+                try {
+                    val candleList = getCandlesUseCase(
+                        instrument.figi,
+                        from = fromCalendar,
+                        to = toCalendar,
+                        interval = timeframe.name,
+                        limit = 1200
+                    )
+                    dispatch(Action.CandlesLoaded(candleList))
+                } catch (e: Exception) {
+                    dispatch(Action.CandlesLoadingError)
+                }
+            }
         }
     }
 
-    private class ExecutorImpl : CoroutineExecutor<Intent, Action, State, Msg, Label>() {
+    private inner class ExecutorImpl : CoroutineExecutor<Intent, Action, State, Msg, Label>() {
+
         override fun executeIntent(intent: Intent) {
+            when (intent) {
+                Intent.ClickBack -> {
+                    publish(Label.ClickBack)
+                }
+
+                Intent.ClickChangeFavouriteStatus -> {
+                    scope.launch {
+                        val state = state()
+                        if (state.isFavourite) {
+                            changeFavouriteStateUseCase.removeFromFavourite(state.instrument.figi)
+                        } else {
+                            changeFavouriteStateUseCase.addToFavourite(state.instrument)
+                        }
+                    }
+                }
+
+                Intent.ClickChangePeriod -> {
+                    dispatch(Msg.PeriodOnChange)
+                }
+
+                Intent.ClickChangeTimeframe -> {
+                    dispatch(Msg.TimeframeOnChange)
+                }
+
+                is Intent.PeriodChanged -> {
+                    dispatch(Msg.PeriodChanged(intent.from, intent.to))
+                }
+
+                is Intent.TimeframeChanged -> {
+                    dispatch(Msg.TimeframeChanged(intent.timeframe))
+                }
+            }
         }
 
         override fun executeAction(action: Action) {
+            when (action) {
+                is Action.CandlesLoaded -> {
+                    dispatch(Msg.CandlesLoaded(action.candlesList))
+                }
+
+                Action.CandlesLoadingError -> {
+                    dispatch(Msg.CandlesLoadingError)
+                }
+
+                Action.CandlesStartLoading -> {
+                    dispatch(Msg.CandlesStartLoading)
+                }
+
+                is Action.FavouriteStatusChanged -> {
+                    dispatch(Msg.FavouriteStatusChanged(action.isFavourite))
+                }
+            }
         }
     }
 
     private object ReducerImpl : Reducer<State, Msg> {
-        override fun State.reduce(message: Msg): State = State(Unit)
+
+        override fun State.reduce(msg: Msg): State = when (msg) {
+            is Msg.CandlesLoaded -> {
+                copy(candlesState = State.CandlesState.Loaded(msg.candlesList))
+            }
+
+            Msg.CandlesLoadingError -> {
+                copy(candlesState = State.CandlesState.Error)
+            }
+
+            Msg.CandlesStartLoading -> {
+                copy(candlesState = State.CandlesState.Loading)
+            }
+
+            is Msg.FavouriteStatusChanged -> {
+                copy(isFavourite = msg.isFavourite)
+            }
+
+            is Msg.PeriodChanged -> {
+                copy(periodState = State.PeriodState.SelectedPeriod(msg.from, msg.to))
+            }
+
+            Msg.PeriodOnChange -> {
+                copy(periodState = State.PeriodState.OnChange)
+            }
+
+            is Msg.TimeframeChanged -> {
+                copy(timeframeState = State.TimeframeState.SelectedTimeframe(msg.timeframe))
+            }
+
+            Msg.TimeframeOnChange -> {
+                copy(timeframeState = State.TimeframeState.OnChange)
+            }
+        }
     }
 }
--- app/src/main/java/ru/skorobogatov/t_investsendbox/presentation/search/SearchStore.kt
+++ app/src/main/java/ru/skorobogatov/t_investsendbox/presentation/search/SearchStore.kt
@@ -5,29 +5,112 @@ import com.arkivanov.mvikotlin.core.store.Store
 import com.arkivanov.mvikotlin.core.store.StoreFactory
 import com.arkivanov.mvikotlin.extensions.coroutines.CoroutineBootstrapper
 import com.arkivanov.mvikotlin.extensions.coroutines.CoroutineExecutor
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import ru.skorobogatov.t_investsendbox.domain.entity.Instrument
+import ru.skorobogatov.t_investsendbox.domain.entity.LastPrice
+import ru.skorobogatov.t_investsendbox.domain.usecase.GetInstrumentInfoUseCase
+import ru.skorobogatov.t_investsendbox.domain.usecase.GetLastPriceUseCase
+import ru.skorobogatov.t_investsendbox.domain.usecase.SearchInstrumentUseCase
 import ru.skorobogatov.t_investsendbox.presentation.search.SearchStore.Intent
 import ru.skorobogatov.t_investsendbox.presentation.search.SearchStore.Label
 import ru.skorobogatov.t_investsendbox.presentation.search.SearchStore.State
+import ru.skorobogatov.t_investsendbox.presentation.search.SearchStore.State.SearchState.*
+import javax.inject.Inject
 
-internal interface SearchStore : Store<Intent, State, Label> {
+interface SearchStore : Store<Intent, State, Label> {
 
     sealed interface Intent {
+
+        data class ChangeSearchQuery(
+            val query: String
+        ) : Intent
+
+        data object ClickBack : Intent
+
+        data object ClickSearch : Intent
+
+        data class ClickInstrument(val instrument: Instrument) : Intent
     }
 
-    data class State(val todo: Unit)
+    data class State(
+        val searchQuery: String,
+        val searchState: SearchState,
+        val instrumentItems: List<InstrumentItem>
+    ) {
+
+        sealed interface SearchState {
+
+            data object Initial : SearchState
+
+            data object Loading : SearchState
+
+            data object Error : SearchState
+
+            data object EmptyResult : SearchState
+
+            data class SuccessLoaded(
+                val figiList: List<String>
+            ) : SearchState
+        }
+
+        data class InstrumentItem(
+            val instrumentState: InstrumentState,
+            val lastPriceState: LastPriceState
+        )
+
+        sealed interface InstrumentState {
+
+            data object Initial : InstrumentState
+
+            data object Loading : InstrumentState
+
+            data object Error : InstrumentState
+
+            data class Loaded(
+                val instrument: Instrument
+            ) : InstrumentState
+        }
+
+        sealed interface LastPriceState {
+
+            data object Initial : LastPriceState
+
+            data object Loading : LastPriceState
+
+            data object Error : LastPriceState
+
+            data class Loaded(
+                val lastPrice: LastPrice
+            ) : LastPriceState
+        }
+    }
 
     sealed interface Label {
+
+        data object ClickBack : Label
+
+        data class ClickInstrumentItem(
+            val instrument: Instrument
+        ) : Label
     }
 }
 
-internal class SearchStoreFactory(
-    private val storeFactory: StoreFactory
+class SearchStoreFactory @Inject constructor(
+    private val storeFactory: StoreFactory,
+    private val getInstrumentInfoUseCase: GetInstrumentInfoUseCase,
+    private val getLastPriceUseCase: GetLastPriceUseCase,
+    private val searchInstrumentUseCase: SearchInstrumentUseCase
 ) {
 
     fun create(): SearchStore =
         object : SearchStore, Store<Intent, State, Label> by storeFactory.create(
             name = "SearchStore",
-            initialState = State(Unit),
+            initialState = State(
+                searchQuery = "",
+                searchState = State.SearchState.Initial,
+                instrumentItems = listOf()
+            ),
             bootstrapper = BootstrapperImpl(),
             executorFactory = ::ExecutorImpl,
             reducer = ReducerImpl
@@ -37,6 +120,26 @@ internal class SearchStoreFactory(
     }
 
     private sealed interface Msg {
+
+        data class ChangeSearchQuery(
+            val query: String
+        ) : Msg
+
+        data object LoadingSearchResult : Msg
+
+        data object SearchResultError : Msg
+
+        data class SearchResultLoaded(
+            val figiList: List<String>
+        ) : Msg
+
+        data object LoadingInstrumentItems : Msg
+
+        data object InstrumentItemsError : Msg
+
+        data class InstrumentItemsLoaded(
+            val instrumentItems: List<State.InstrumentItem>
+        ) : Msg
     }
 
     private class BootstrapperImpl : CoroutineBootstrapper<Action>() {
@@ -44,15 +147,115 @@ internal class SearchStoreFactory(
         }
     }
 
-    private class ExecutorImpl : CoroutineExecutor<Intent, Action, State, Msg, Label>() {
+    private inner class ExecutorImpl : CoroutineExecutor<Intent, Action, State, Msg, Label>() {
+
+        private var searchJob: Job? = null
+
         override fun executeIntent(intent: Intent) {
+            when (intent) {
+                is Intent.ChangeSearchQuery -> {
+                    dispatch(Msg.ChangeSearchQuery(intent.query))
+                }
+
+                Intent.ClickBack -> {
+                    publish(Label.ClickBack)
+                }
+
+                is Intent.ClickInstrument -> {
+                    publish(Label.ClickInstrumentItem(intent.instrument))
+                }
+
+                Intent.ClickSearch -> {
+                    searchJob?.cancel()
+                    searchJob = scope.launch {
+                        dispatch(Msg.LoadingSearchResult)
+                        try {
+                            val figiList = searchInstrumentUseCase(state().searchQuery)
+                            dispatch(Msg.SearchResultLoaded(figiList))
+                            dispatch(Msg.LoadingInstrumentItems)
+                            try {
+                                val instrumentItems = figiList.map {
+                                    State.InstrumentItem(
+                                        instrumentState = try {
+                                            State.InstrumentState.Loaded(loadInstrument(it))
+                                        } catch (e: Exception) {
+                                            State.InstrumentState.Error
+                                        },
+                                        lastPriceState = try {
+                                            State.LastPriceState.Loaded(loadLastPrice(it))
+                                        } catch (e: Exception) {
+                                            State.LastPriceState.Error
+                                        }
+                                    )
+                                }
+                                dispatch(Msg.InstrumentItemsLoaded(instrumentItems))
+                            } catch (e: Exception) {
+                                dispatch(Msg.InstrumentItemsError)
+                            }
+
+
+                        } catch (e: Exception) {
+                            dispatch(Msg.SearchResultError)
+                        }
+                    }
+                }
+            }
         }
 
-        override fun executeAction(action: Action) {
+        private suspend fun loadInstrument(figi: String): Instrument {
+            return getInstrumentInfoUseCase(figi)
+        }
+
+        private suspend fun loadLastPrice(figi: String): LastPrice {
+            return getLastPriceUseCase(listOf(figi)).first()
         }
     }
 
     private object ReducerImpl : Reducer<State, Msg> {
-        override fun State.reduce(message: Msg): State = State(Unit)
+
+        override fun State.reduce(msg: Msg): State = when (msg) {
+
+            is Msg.ChangeSearchQuery -> {
+                copy(searchQuery = msg.query)
+            }
+
+            Msg.LoadingSearchResult -> {
+                copy(searchState = Loading)
+            }
+
+            Msg.SearchResultError -> {
+                copy(searchState = Error)
+            }
+
+            is Msg.SearchResultLoaded -> {
+                if (msg.figiList.isEmpty()) {
+                    copy(searchState = EmptyResult)
+                } else {
+                    copy(searchState = SuccessLoaded(msg.figiList))
+                }
+            }
+
+            Msg.InstrumentItemsError -> {
+                copy(instrumentItems = instrumentItems.map {
+                    it.copy(
+                        instrumentState = State.InstrumentState.Error,
+                        lastPriceState = State.LastPriceState.Error
+                    )
+                })
+            }
+
+            is Msg.InstrumentItemsLoaded -> {
+                copy(instrumentItems = msg.instrumentItems)
+            }
+
+            Msg.LoadingInstrumentItems -> {
+                copy(instrumentItems = instrumentItems.map {
+                    it.copy(
+                        instrumentState = State.InstrumentState.Loading,
+                        lastPriceState = State.LastPriceState.Loading
+                    )
+                })
+            }
+        }
     }
 }
