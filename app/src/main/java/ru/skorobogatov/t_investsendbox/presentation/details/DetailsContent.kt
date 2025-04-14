121f9e48d742341dddbba38e5b0f65a9fe34a5f8
Abbat888 <zmeuka96@gmail.com>
2025-04-14 23:26:47 +0400

    DetailsContent.kt - work in progress added datePicker and menu for timeFrames

--- app/src/main/java/ru/skorobogatov/t_investsendbox/presentation/details/DetailsContent.kt
+++ app/src/main/java/ru/skorobogatov/t_investsendbox/presentation/details/DetailsContent.kt
@@ -1,16 +1,12 @@
 package ru.skorobogatov.t_investsendbox.presentation.details
 
-import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -21,12 +17,18 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.Timelapse
 import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -35,16 +37,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.center
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.text.TextMeasurer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ru.skorobogatov.t_investsendbox.domain.entity.Candle
-import kotlin.math.absoluteValue
+import ru.skorobogatov.t_investsendbox.domain.entity.Timeframe
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -52,6 +61,11 @@ fun DetailsContent(
     component: DetailsComponent
 ) {
     val state by component.model.collectAsState()
+
+    var expend by remember { mutableStateOf(false) }
+
+    var openDialog by remember { mutableStateOf(false) }
+    var dateRangePickerState = rememberDateRangePickerState()
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -111,11 +125,74 @@ fun DetailsContent(
 
             is DetailsStore.State.CandlesState.Loaded -> {
                 Terminal(
-                    candleList = candleState.candlesList
+                    candleList = candleState.candlesList.reversed()
                 )
             }
 
             DetailsStore.State.CandlesState.Loading -> {
+
+            }
+        }
+        when(state.timeframeState){
+            DetailsStore.State.TimeframeState.OnChange -> {
+                expend = true
+                DropdownMenu(
+                    expanded = expend,
+                    onDismissRequest = { expend = false },
+                    modifier = Modifier
+                ) {
+                    Timeframe.entries.forEachIndexed { index, timeFrame ->
+                        if (index != 0){
+                            DropdownMenuItem(
+                                text = { Text(text = timeFrame.name.removePrefix("CANDLE_INTERVAL_")) },
+                                onClick = {
+                                    component.onTimeFrameChanged(timeFrame)
+                                    expend = false
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+            is DetailsStore.State.TimeframeState.SelectedTimeframe -> {
+
+            }
+        }
+        when(state.periodState){
+            DetailsStore.State.PeriodState.OnChange -> {
+                openDialog = true
+                DatePickerDialog(
+                    onDismissRequest = { openDialog = false },
+                    confirmButton = {
+                        TextButton(
+                            onClick = {
+                                val from = dateRangePickerState.selectedStartDateMillis
+                                val to = dateRangePickerState.selectedEndDateMillis
+                                if (from != null && to != null){
+                                    component.onPeriodChanged(
+                                        from = from,
+                                        to = to
+                                    )
+                                }
+                                openDialog = false
+                            }
+                        ) { Text(text = "Применить") }
+                    },
+                    dismissButton = {
+                        TextButton(
+                            onClick = {
+                                openDialog = false
+                            }
+                        ) { Text(text = "Отмена") }
+                    }
+                ) {
+                    DateRangePicker(
+                        state = dateRangePickerState,
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+            }
+            is DetailsStore.State.PeriodState.SelectedPeriod -> {
 
             }
         }
@@ -137,6 +214,8 @@ fun Terminal(
     var minPrice by remember { mutableStateOf(1f) }
     var pxPerPoint by remember { mutableStateOf(1f) }
 
+    val textMeasurer = rememberTextMeasurer()
+
     LaunchedEffect(
         key1 = visibleItemsInfo.value
     ) {
@@ -147,78 +226,168 @@ fun Terminal(
         minPrice = visibleCandles.minOfOrNull { it.low } ?: 0f
         priceRange = (maxPrice - minPrice)
         pxPerPoint = height / priceRange
-        Log.d(
-            "price",
-            "priceRange = $priceRange, maxPrice = $maxPrice, minPrice = $minPrice, pixelPerPrice = $pxPerPoint, height = $height"
-        )
     }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
-            .onGloballyPositioned {
-                height = it.size.height
-            }
-    ) {
-        LazyRow(
+    ){
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .pointerInput(Unit) {
-                    detectTransformGestures { _, _, zoom, _ ->
-                        scale *= zoom
-                        scale = scale.coerceIn(0.5f, 3f)
-                    }
-                },
-            state = listState,
-            reverseLayout = true
+                .background(Color.Black)
+                .padding(top = 128.dp, bottom = 48.dp)
+                .onGloballyPositioned {
+                    height = it.size.height
+                }
         ) {
-            items(
-                items = candleList,
-                key = { it.toString() },
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .pointerInput(Unit) {
+                        detectTransformGestures { _, _, zoom, _ ->
+                            scale *= zoom
+                            scale = scale.coerceIn(0.5f, 3f)
+                        }
+                    },
+                state = listState,
+                reverseLayout = true
             ) {
-                Log.d(
-                    "candle",
-                    "candle.high = ${it.high}, candle.low = ${it.low}, candle.open = ${it.open}, candle.close = ${it.close}"
-                )
-
-                val wickHeight = (it.high - it.low) * pxPerPoint
-                val bodyHeight = (it.open - it.close).absoluteValue * pxPerPoint
-
-                val wickOffset = (maxPrice - it.high) * pxPerPoint
-                val bodyOffset = (maxPrice - maxOf(it.open, it.close)) * pxPerPoint
-
-                Log.d(
-                    "item",
-                    "wickHeight = $wickHeight, bodyHeight = $bodyHeight, wickOffset = $wickOffset, bodyOffset = $bodyOffset"
-                )
-
-                Box(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .padding(2.dp)
-                        .width((8 * scale).dp)
+                items(
+                    items = candleList,
+                    key = { it.toString() },
                 ) {
-                    Canvas(
-                        modifier = Modifier.fillMaxSize()
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(1.dp)
+                            .width((8 * scale).dp)
                     ) {
-                        drawLine(
-                            color = Color.White,
-                            start = Offset(size.center.x, (maxPrice - it.high) * pxPerPoint),
-                            end = Offset(size.center.x, (maxPrice - it.low) * pxPerPoint),
-                            strokeWidth = 2f * scale
-                        )
-                        drawLine(
-                            color = if (it.open > it.close) Color.Red else Color.Green,
-                            start = Offset(size.center.x, (maxPrice - maxOf(it.open, it.close)) * pxPerPoint),
-                            end = Offset(size.center.x, (maxPrice - minOf(it.open, it.close)) * pxPerPoint),
-                            strokeWidth = 8f * scale
-                        )
+                        Canvas(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            drawLine(
+                                color = if (it.open > it.close) Color.Red else Color.Green,
+                                start = Offset(size.center.x, (maxPrice - it.high) * pxPerPoint),
+                                end = Offset(size.center.x, (maxPrice - it.low) * pxPerPoint),
+                                strokeWidth = 4f * scale
+                            )
+                            drawLine(
+                                color = if (it.open > it.close) Color.Red else Color.Green,
+                                start = Offset(
+                                    size.center.x,
+                                    (maxPrice - maxOf(it.open, it.close)) * pxPerPoint
+                                ),
+                                end = Offset(
+                                    size.center.x,
+                                    (maxPrice - minOf(it.open, it.close)) * pxPerPoint
+                                ),
+                                strokeWidth = 16f * scale
+                            )
+                        }
                     }
                 }
             }
+
+        }
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .clipToBounds()
+                .padding(top = 128.dp, bottom = 48.dp)
+        ) {
+            val lastPrice = if (candleList.first().open > candleList.first().close) candleList.first().open else candleList.first().close
+            drawPrices(
+                max = maxPrice,
+                min = minPrice,
+                pxPerPoint = pxPerPoint,
+                lastPrice = lastPrice,
+                textMeasurer = textMeasurer
+            )
         }
     }
+}
+
+private fun DrawScope.drawPrices(
+    max: Float,
+    min: Float,
+    pxPerPoint: Float,
+    lastPrice: Float,
+    textMeasurer: TextMeasurer
+) {
+    //max price
+    val maxPriceOffsetY = 0f
+    drawDashedLine(
+        start = Offset(0f, maxPriceOffsetY),
+        end = Offset(size.width, maxPriceOffsetY)
+    )
+    drawTextPrice(
+        textMeasurer = textMeasurer,
+        price = max,
+        offsetY = maxPriceOffsetY
+    )
+
+    //last price
+    val lastPriceOffsetY = size.height - ((lastPrice - min) * pxPerPoint)
+    drawDashedLine(
+        start = Offset(0f, lastPriceOffsetY),
+        end = Offset(size.width, lastPriceOffsetY)
+    )
+    drawTextPrice(
+        textMeasurer = textMeasurer,
+        price = lastPrice,
+        offsetY = lastPriceOffsetY
+    )
+
+    //min price
+    val minPericeOffsetY = size.height
+    drawDashedLine(
+        start = Offset(0f, minPericeOffsetY),
+        end = Offset(size.width, minPericeOffsetY)
+    )
+    drawTextPrice(
+        textMeasurer = textMeasurer,
+        price = min,
+        offsetY = minPericeOffsetY
+    )
+}
+
+private fun DrawScope.drawTextPrice(
+    textMeasurer: TextMeasurer,
+    price: Float,
+    offsetY: Float
+) {
+    val textLayoutResult = textMeasurer.measure(
+        text = price.toString(),
+        style = TextStyle(
+            color = Color.White,
+            fontSize = 12.sp
+        )
+    )
+    drawText(
+        textLayoutResult = textLayoutResult,
+        topLeft = Offset(size.width - textLayoutResult.size.width - 4.dp.toPx(), offsetY)
+    )
+}
+
+private fun DrawScope.drawDashedLine(
+    color: Color = Color.White,
+    start: Offset,
+    end: Offset,
+    strokeWidth: Float = 1f
+) {
+    drawLine(
+        color = color,
+        start = start,
+        end = end,
+        strokeWidth = strokeWidth,
+        pathEffect = PathEffect.dashPathEffect(
+            intervals = floatArrayOf(
+                4.dp.toPx(), 4.dp.toPx()
+            )
+        )
+    )
 }
 
 
